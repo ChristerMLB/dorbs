3d71a1a7c134df30f2991942bf6f50a16fe22241
Christer M.L. Bendixen <git@fortelle.no>
2025-08-31 12:58:05 +0200

    making the test page mobile friendly

--- javascript.js
+++ javascript.js
@@ -4,19 +4,19 @@ import { GLTFLoader } from "./GLTFLoader.js"
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
-  600 / 400,
+  320 / 300,
   0.1,
   1000
 );
 
 const renderer = new THREE.WebGLRenderer({ alpha: true });
-renderer.setSize(600, 400);
+renderer.setSize(320, 300);
 renderer.setClearColor(0x000000, 0);
 document.getElementById("threedee").appendChild(renderer.domElement);
 
 camera.position.z = 2.5;
 
-const light = new THREE.DirectionalLight(0xca1800, 1);
+const light = new THREE.DirectionalLight(0xf5deb3, 1);
 light.position.set(1, 1, 2).normalize();
 scene.add(light);
 
